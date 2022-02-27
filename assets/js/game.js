c1b03fe772b598f2e85b66cf549dabae581fac1f
bradyk <brady1187cool@gmail.com>
2022-02-26 20:28:12 -0600

    starting game function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,26 @@
-window.alert("Check out this alert")
+const playerName = window.prompt("What is your robot's name?");
+let playerHealth = 100;
+let playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+const enemyName = "Weirdo";
+let enemyHealth = 50;
+let enemyAttack = 12;
+
+const fight = function() {
+    window.alert("Welcome to Robot Brawl!")
+
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    
+        
+    )
+        //This is shorter, faster option. USE BACK TICKS
+        //console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining.`)
+        playerHealth = playerHealth - enemyAttack;
+        console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`)
+};
+
+fight();
